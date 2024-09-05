a1dbf6fbf950d860652d367773c8543d4819c7bb
Kirill <blacklight310398@gmail.com>
2024-09-05 13:29:53 +0300

    Initial commit

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lab3;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvInfo;
     EditText etInput;
-    Button btnInput;
-    int targetNumber;
+    Button btnInput, btnEasy, btnNormal, btnHard;
+    int targetNumber, max = 50;
     boolean isFinished;
 
     @Override
@@ -37,16 +38,21 @@ public class MainActivity extends AppCompatActivity {
         tvInfo = findViewById(R.id.tvInfo);
         etInput = findViewById(R.id.etInput);
         btnInput = findViewById(R.id.btnInput);
+        btnEasy = findViewById(R.id.btnEasy);
+        btnNormal = findViewById(R.id.btnNormal);
+        btnHard = findViewById(R.id.btnHard);
 
         targetNumber = generateRandom(0,50);
         isFinished = false;
+
+        btnEasy.setOnClickListener(changeDifficulty);
+        btnNormal.setOnClickListener(changeDifficulty);
+        btnHard.setOnClickListener(changeDifficulty);
     }
-
-
 
     public void onClick(View v) {
         if (isFinished) {
-            targetNumber = generateRandom(0, 50);
+            targetNumber = generateRandom(0, max);
             btnInput.setText(R.string.input_value);
         }
 
@@ -54,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
         if (inputText.matches("")) Toast.makeText(getApplicationContext(), R.string.error, Toast.LENGTH_LONG).show();
         else {
             int guessedNumber = Integer.parseInt(inputText);
-
             if (guessedNumber < targetNumber) tvInfo.setText(R.string.behind);
             else if (guessedNumber > targetNumber) tvInfo.setText(R.string.ahead);
             else {
@@ -65,8 +70,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    View.OnClickListener changeDifficulty = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btnEasy:
+                max = 25;
+                tvInfo.setText(R.string.try_to_guess_easy);
+                break;
+            case R.id.btnNormal:
+                max = 50;
+                tvInfo.setText(R.string.try_to_guess);
+                break;
+            case R.id.btnHard:
+                max = 100;
+                tvInfo.setText(R.string.try_to_guess_hard);
+                break;
+        }
+
+            targetNumber = generateRandom(0, max);
+            isFinished = false;
+            btnInput.setText(R.string.input_value);
+        }
+    };
+
     public int generateRandom(int min, int max){
         Random random = new Random();
         return random.ints(min, max).findFirst().getAsInt();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        targetNumber = generateRandom(0,50);
+        isFinished = false;
+        tvInfo.setText(R.string.try_to_guess);
+        btnInput.setText(R.string.input_value);
     }
 }
